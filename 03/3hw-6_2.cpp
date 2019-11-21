9e8a6ccf00c79220e32fa5c276efc490b4e3da51
Artem <artemkarablinov@gmail.com>
2019-11-21 12:12:19 +0300

    6_2 + 6_1(preorder print func)

--- 03/3hw-6_2.cpp
+++ 03/3hw-6_2.cpp
@@ -1,6 +1,130 @@
-#include <iostream>
+/*
+
+
+Дано число N ≤ 10^4 и последовательность целых чисел из [-2^31..2^31] длиной N. Требуется построить бинарное дерево, заданное наивным порядком вставки. 
+Т.е., при добавлении очередного числа K в дерево с корнем root, если root→Key ≤ K, то узел K добавляется в правое поддерево root; иначе в левое поддерево root. 
+Выведите элементы в порядке in-order (слева направо).
+Рекурсия запрещена.
+
+*/
+
+#include<iostream>
+#include<stack>
+
+struct Node{
+    Node* left_child = nullptr;
+    Node* right_child = nullptr;
+    int key;
+    Node(int data){key = data;};
+};
+
+class Tree{
+    public:
+        void add(int data);
+        void inorder_print();
+        void inorder_print(Node* leaf);
+
+        void preorder_print();
+        void preorder_print(Node* leaf);
+        Tree();
+        ~Tree(){};
+    private:
+        Node* root;
+};
+
+Tree::Tree(){
+    root = nullptr;
+}
+
+void Tree:: add(int data){
+    if (!root){
+        root = new Node(data);
+        return;
+    }
+    Node* current = root;
+    Node* parent = root;
+    while(current){
+        parent = current;
+        if (data < current->key){
+            current = current->left_child;
+        }
+        else{
+            current = current->right_child;
+        }
+    }
+    Node* newnode = new Node(data);
+
+    if (data < parent->key)
+        parent->left_child = newnode;
+    else
+        parent->right_child = newnode;
+};
+
+void Tree::inorder_print(){
+    inorder_print(root);
+    std:: cout << std::endl;
+}
+
+void Tree::inorder_print(Node* root){
+    if (!root){
+        return;
+    };
+
+    std:: stack <Node*> s;
+    Node* current = root;
+
+    while(!s.empty() || current){
+
+        while(current){
+            s.push(current);
+            current = current->left_child;    
+        }
+
+        current = s.top();
+        s.pop();
+        std:: cout<<current->key<<" ";
+
+        current = current-> right_child;
+    }
+}
+
+void Tree::preorder_print(){
+    preorder_print(root);
+    std:: cout << std::endl;
+}
+
+void Tree::preorder_print(Node *root){
+    if (!root)
+    {
+       return;
+    }
+    std:: stack<Node *> s; 
+    s.push(root);  
+    while (!s.empty())
+    {
+        Node *temp = s.top();
+        s.pop();
+        std::cout << temp->key << " ";
+        if (temp->right_child)
+            s.push(temp->right_child);
+        if (temp->left_child)
+            s.push(temp->left_child); 
+    }
+}
 
 
 int main(){
+    int n;
+    std:: cin >> n;
+    Tree mytree;
+    for (int i=0; i<n; i++){
+        int data;
+        std:: cin >> data;
+        mytree.add(data);
+    };
+    mytree.preorder_print();
+    mytree.inorder_print();
+    
+    
 	return 0;
 }
