0717660f86b6c210edf59572a788c59edbc4de70
Artem <artemkarablinov@gmail.com>
2019-12-16 19:48:28 +0300

    new idea

--- 06/bridges_12_ex.cpp
+++ 06/bridges_12_ex.cpp
@@ -5,6 +5,18 @@
 #include <unordered_map>
 #include <fstream>
 
+std::set<int>* DFS(std::vector<std::vector<int>> edges){
+    int time = 0;
+    std::vector<bool> isVisited(edges.size(), false);
+    std::vector<size_t> color(edges.size(), 0);
+
+
+
+}
+
+
+
+
 int main(){
     std::ifstream input("bridges.in");
     std::ofstream output("bridges.out");
@@ -13,13 +25,14 @@ int main(){
     std::in >> n >> k;
 
     std::vector<std::vector<int>> graph(n);
-    std::unordered_map<std::string, std::pair<int, bool> > edges{};
-    set<int> b; // set of bridges
-    for (auto i=0; i < k; i++){
+    std::set<int>* b; // ptr on set of bridges
+    for (int i=0; i <= k; i++){
         int v1, v2; // вершины
         std::in >> v1 >> v2;
-        
+        graph[v1].push_back(v2);
+        graph[v2].push_back(v1);
     }
+    b = DFS(graph);
 }
 
 
