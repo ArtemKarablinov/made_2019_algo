eb55805c6df888de0b45bab068a8fac7fe188923
Artem <artemkarablinov@gmail.com>
2019-12-16 23:01:47 +0300

    new idea

--- 06/bridges_12_ex.cpp
+++ 06/bridges_12_ex.cpp
@@ -5,34 +5,66 @@
 #include <unordered_map>
 #include <fstream>
 
-std::set<int>* DFS(std::vector<std::vector<int>> edges){
-    int time = 0;
-    std::vector<bool> isVisited(edges.size(), false);
-    std::vector<size_t> color(edges.size(), 0);
+using std::vector;
 
-
+void bridge_dfs(int v, 
+                int p,
+                int timer, 
+                vector<vector<int>> &edges, 
+                vector<bool> &isVisited, 
+                vector<int> &tin, 
+                vector<int> &fup){
+    isVisited[v] = true;
+    tin[v] = fup[v] = timer++;
+    for (int i=0; edges[v].size(); ++i){
+        int to = edges[v][i];
+        if (to==p)
+            continue;
+        if (isVisited[to])
+            fup[v] = fup[v]<tin[to] ? fup[v]:tin[to];
+        else {
+            bridge_dfs(to, v, timer, edges, isVisited, tin, fup);
+            fup[v] = fup[v]<fup[to] ? fup[v]:fup[to];
+            if (fup[to] > tin[v])
+                std::cout<< v << to <<std::endl;
+        }
+    }
 
 }
 
 
+void solve_bridges(std::vector<std::vector<int>> &edges){
+    int timer = 0;
+    int p = -1;
+    std::vector<int> enter(edges.size());
+    std::vector<int> ret(edges.size());
+    // std::vector<size_t> color(edges.size(), 0);
+    std::vector<bool> isVisited(edges.size(), false);
+    for (int i=0; i<edges.size(); i++)
+        if (!isVisited[i])
+            bridge_dfs(i, p, timer, edges, isVisited, enter, ret);
+}
+
 
 
 int main(){
-    std::ifstream input("bridges.in");
-    std::ofstream output("bridges.out");
+    // std::ifstream input("bridges.in");
+    // std::ofstream output("bridges.out");
     //число вершин и ребер
     int n, k;
-    std::in >> n >> k;
+    // input >> n >> k;
 
-    std::vector<std::vector<int>> graph(n);
-    std::set<int>* b; // ptr on set of bridges
-    for (int i=0; i <= k; i++){
-        int v1, v2; // вершины
-        std::in >> v1 >> v2;
-        graph[v1].push_back(v2);
-        graph[v2].push_back(v1);
+    std::cin >> n >> k;
+    std::vector<std::vector<int>> edges(k);
+    // std::set<int>* b; // ptr on set of bridges
+    int v1, v2; // вершины
+    for (int i=0; i < k; i++){
+        //input >> v1 >> v2;
+        std::cin>>v1 >> v2;
+        edges[v1].push_back(v2);
+        edges[v2].push_back(v1);
     }
-    b = DFS(graph);
+    solve_bridges(edges);
 }
 
 
